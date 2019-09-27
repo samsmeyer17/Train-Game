18cdd7463aa8e89c5aed83025ef2427723f76b1d
Samuel Meyer <sam.smeyer17@gmail.com>
2019-09-27 10:57:03 -0700

    trying to get the firebase connection active. Currently not logging and coming up with an error that says it can't find the file when i go to input data

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,98 @@
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyAVsTh-a8XkebmRd_6Lq7g0oQcBjd66fTA",
-    authDomain: "train-game-44ffb.firebaseapp.com",
-    databaseURL: "https://train-game-44ffb.firebaseio.com",
-    projectId: "train-game-44ffb",
-    storageBucket: "train-game-44ffb.appspot.com",
-    messagingSenderId: "1002607826901",
-    appId: "1:1002607826901:web:e801eaba471c4788"
+  // Your web app's Firebase configuration
+  var config = {
+    apiKey: "AIzaSyD5lKxHFd7-oXQl-nqeUWA_M10yJx902uk",
+    authDomain: "train-game-dbe5a.firebaseapp.com",
+    databaseURL: "https://train-game-dbe5a.firebaseio.com",
+    projectId: "train-game-dbe5a",
+    storageBucket: "",
+    messagingSenderId: "794839378758",
+    appId: "1:794839378758:web:41c977d6d4fc050ffb5347",
+    measurementId: "G-CPL17Q6HJ3"
   };
-  firebase.initializeApp(firebaseConfig);
-
-  var trainData = firebase.database();
+  // Initialize Firebase
+  firebase.initializeApp(config);
+  firebase.analytics();
   
-$("#add-train-btn").on("click", function (event) {
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrain = $("#first-train-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
   
-  event.preventDefault();
-
+    var newTrain = {
+      name: trainName,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency
+    };
   
-  var trainName = $("#train-name-input")
-    .val()
-    .trim();
-  var destination = $("#destination-input")
-    .val()
-    .trim();
-  var firstTrain = $("#first-train-input")
-    .val()
-    .trim();
-  var frequency = $("#frequency-input")
-    .val()
-    .trim();
-
-  var newTrain = {
-    name: trainName,
-    destination: destination,
-    firstTrain: firstTrain,
-    frequency: frequency
-  };
-
-  trainData.ref().push(newTrain);
-
-  console.log(newTrain.name);
-  console.log(newTrain.destination);
-  console.log(newTrain.firstTrain);
-  console.log(newTrain.frequency);
-
-  alert("Train successfully added");
-
-  // Clears all of the text-boxes
-  $("#train-name-input").val("");
-  $("#destination-input").val("");
-  $("#first-train-input").val("");
-  $("#frequency-input").val("");
-});
-
-
-trainData.ref().on("child_added", function (childSnapshot, prevChildKey) {
-  console.log(childSnapshot.val());
-
-  var tName = childSnapshot.val().name;
-  var tDestination = childSnapshot.val().destination;
-  var tFrequency = childSnapshot.val().frequency;
-  var tFirstTrain = childSnapshot.val().firstTrain;
-
-  var timeArr = tFirstTrain.split(":");
-  var trainTime = moment()
-    .hours(timeArr[0])
-    .minutes(timeArr[1]);
-  var maxMoment = moment.max(moment(), trainTime);
-  var tMinutes;
-  var tArrival;
-
-  if (maxMoment === trainTime) {
-    tArrival = trainTime.format("hh:mm A");
-    tMinutes = trainTime.diff(moment(), "minutes");
-  } else {
-    var differenceTimes = moment().diff(trainTime, "minutes");
-    var tRemainder = differenceTimes % tFrequency;
-    tMinutes = tFrequency - tRemainder;
-    tArrival = moment()
-      .add(tMinutes, "m")
-      .format("hh:mm A");
-  }
-  console.log("tMinutes:", tMinutes);
-  console.log("tArrival:", tArrival);
+    // Uploads train data to the database
+    trainData.ref().push(newTrain);
   
-  $("#train-table > tbody").append(
-    $("<tr>").append(
-      $("<td>").text(tName),
-      $("<td>").text(tDestination),
-      $("<td>").text(tFrequency),
-      $("<td>").text(tArrival),
-      $("<td>").text(tMinutes)
-    )
-  );
-});
+    // Logs everything to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
+  
+    // Alert
+    alert("Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-input").val("");
+    $("#frequency-input").val("");
+  });
+  
+  // 4. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
+  trainData.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var tName = childSnapshot.val().name;
+    var tDestination = childSnapshot.val().destination;
+    var tFrequency = childSnapshot.val().frequency;
+    var tFirstTrain = childSnapshot.val().firstTrain;
+  
+    var timeArr = tFirstTrain.split(":");
+    var trainTime = moment()
+      .hours(timeArr[0])
+      .minutes(timeArr[1]);
+    var maxMoment = moment.max(moment(), trainTime);
+    var tMinutes;
+    var tArrival;
+  
+    // If the first train is later than the current time, sent arrival to the first train time
+    if (maxMoment === trainTime) {
+      tArrival = trainTime.format("hh:mm A");
+      tMinutes = trainTime.diff(moment(), "minutes");
+    } else {
+      // Calculate the minutes until arrival using hardcore math
+      // To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time
+      // and find the modulus between the difference and the frequency.
+      var differenceTimes = moment().diff(trainTime, "minutes");
+      var tRemainder = differenceTimes % tFrequency;
+      tMinutes = tFrequency - tRemainder;
+      // To calculate the arrival time, add the tMinutes to the current time
+      tArrival = moment()
+        .add(tMinutes, "m")
+        .format("hh:mm A");
+    }
+    console.log("tMinutes:", tMinutes);
+    console.log("tArrival:", tArrival);
+  
+    // Add each train's data into the table
+    $("#train-table > tbody").append(
+      $("<tr>").append(
+        $("<td>").text(tName),
+        $("<td>").text(tDestination),
+        $("<td>").text(tFrequency),
+        $("<td>").text(tArrival),
+        $("<td>").text(tMinutes)
+      )
+    );
+  });
